22eda4e75312979aa7432c08fe66356b5d459bba
Pia Soy <piasoy4@gmail.com>
2019-10-15 08:44:58 -0500

    add scroll functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,15 @@
 let skillArr = document.getElementsByClassName("skill");
- let learnArr = document.getElementsByClassName("learn");
-//  let begin = document.getElementById("scroll");
-//   document.getElementById("enter").onscroll = function(){
-//   begin.onclick = function() {
-//         skills.scrollIntoView();
-//         for(let i=0; i<skillArr.length; i++){
-//             skillArr[i].classList.remove("none");  
-//             skillArr[i].classList.add("roll-in-left")
-//         }
-//         for(let i=0; i<learnArr.length; i++){
-//             learnArr[i].classList.remove("none");
-//             learnArr[i].classList.add("roll-in-right");
-//         }
-    
-//    }
+let learnArr = document.getElementsByClassName("learn");
+
+
+$(window).scroll(function(){
+    console.log('passed div')
+    console.log(skillArr)
+    console.log(skillArr[0].className)
+   if(skillArr[0].className === 'skill none'){
+        mySkills()
+   }
+  });
 
 function mySkills() {
             skills.scrollIntoView();
